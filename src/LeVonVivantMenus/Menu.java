6b1a386075b32268cff72a74269b64fb82097e9d
JoelG <JoelG@Joel-PC>
2020-03-29 22:47:00 -0600

    sincronizado

---
+++ src/LeVonVivantMenus/Menu.java
@@ -0,0 +1,16 @@
+package LeVonVivantMenus;
+
+public class Menu {
+    
+    public void MenuPrincipal(){
+        System.out.println("===== Cafeteria LeVonViVant =====");
+        System.out.println("Gusto en atenderlo... ¿Qué le puedo ofrecer?");
+        System.out.println("=o=o=o=o=o=o=o=o=o=");
+        System.out.println("1.Cafes");
+        System.out.println("2.Postres");
+        System.out.println("3.Ver orden");
+        System.out.println("4.Finalizar Orden");
+        System.out.println("=o=o=o=o=o=o=o=o=o=");
+        System.out.println("Porfavor indique el numero de la orden que desea en su teclado");
+    }
+}
